5dcaf11bd0eb40d0aeca0fcaa33c28a67fba1381
pep <borrellpep@gmail.com>
2023-06-06 17:31:22 +0200

    fix: better printing

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 from games import ModifiedRockPaperScissors, PrisonersDilemma
 from grid import Grid, GridVisualizer
@@ -6,9 +7,7 @@ from grid import Grid, GridVisualizer
 if __name__ == "__main__":
     np.random.seed(0)
     grid = Grid(game=ModifiedRockPaperScissors)
-    for i in range(50):
-        print(f"Step {i}")
+    for i in tqdm(range(50)):
         grid.step()
         vis = GridVisualizer(grid)
         vis.visualize()
-    print(vis.strategy_grid())
